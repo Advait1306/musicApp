3dbaa91fe1d17dfc4cff777c8ad5d748d289976b
Ikjot <ikjotkhurana@gmail.com>
2020-05-27 00:34:16 +0530

    RoomDb Code: Song class made RoomDb Code: SongDAO interface made RoomDb Code: SongDatabase abstract class made

--- app/src/main/java/com/napster/musicapp/database/SongDatabase.kt
+++ app/src/main/java/com/napster/musicapp/database/SongDatabase.kt
@@ -1,2 +1,50 @@
 package com.napster.musicapp.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = arrayOf(Song::class), version = 1)
+abstract class SongDatabase : RoomDatabase() {
+
+    abstract fun songDAO(): SongDAO
+
+
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SongDatabase? = null
+
+        fun getDatabase(context: Context): SongDatabase {
+            val tempInstance = INSTANCE
+
+            if (tempInstance != null) {
+                return tempInstance
+            } else {
+                synchronized(this) {
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SongDatabase::class.java,
+                        "song"
+                    ).build()
+                    INSTANCE = instance
+                    return instance
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+                }
+
+
+
+
+}
