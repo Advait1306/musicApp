7a49861c285c6b457f80aa64cf6b895402eb5dd4
Advait Bansode <advaitbansode4@gmail.com>
2020-05-26 10:27:41 +0530

    Fixed SongRepository code and refactored it too.

--- app/src/main/java/com/napster/musicapp/MainActivity.kt
+++ app/src/main/java/com/napster/musicapp/MainActivity.kt
@@ -2,20 +2,11 @@ package com.napster.musicapp
 
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
-import android.Manifest.*
 import android.Manifest.permission.READ_EXTERNAL_STORAGE as re
-import android.content.pm.PackageManager
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import android.os.Parcel
-import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.napster.musicapp.repositories.songList
-import com.napster.musicapp.repositories.Populate
 import kotlin.properties.Delegates
-import com.napster.musicapp.repositories.Songdata
-import com.napster.musicapp.repositories.Songfileinfo
+import com.napster.musicapp.repositories.SongRepository
 
 class MainActivity : AppCompatActivity()  {
 
@@ -30,18 +21,10 @@ class MainActivity : AppCompatActivity()  {
                 mediaPlayer = MediaPlayer.create(applicationContext, R.raw.song)
 
             }
-
             mediaPlayer!!.start()
         }
-        val songfileinfo :Songfileinfo = Songfileinfo(Parcel.obtain())
-        Populate().audioRet()
-        Songdata().getPlayList(this)
-        var perm by Delegates.notNull<Int>()
         requestPermissions(arrayOf(re),12345)
-        perm = checkSelfPermission(re)
-        Log.d("SHERE","${songfileinfo.title}")
-        Log.d("HERE", "$songList")
-        Log.d("THERE",perm.toString())
+        SongRepository().getPlayList(applicationContext)
 
     }
 }
---
+++ app/src/main/java/com/napster/musicapp/repositories/Song.kt
@@ -0,0 +1,14 @@
+package com.napster.musicapp.repositories
+
+import android.os.Parcel
+
+
+class Song() {
+    var title: String? = null
+    var album: String? = null
+    var artist: String? = null
+    var fileUri: String? = null
+    var albumArt: String? = null
+    var duration: String? = null
+    var id: Long = 0
+}
