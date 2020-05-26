786427d6c57fdbd4f9f083bdcf84492491bce3d7
garvj <lovertech1102@gmail.com>
2020-05-27 01:29:10 +0530

    Permissions Handled

--- app/src/main/java/com/napster/musicapp/MainActivity.kt
+++ app/src/main/java/com/napster/musicapp/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.napster.musicapp
 
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.Manifest.permission.READ_EXTERNAL_STORAGE as re
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.properties.Delegates
-import com.napster.musicapp.repositories.SongRepository
 import com.napster.musicapp.viewModels.SongListViewModel
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,17 +18,40 @@ class MainActivity : AppCompatActivity()  {
         var mediaPlayer: MediaPlayer? = null
 
         play.setOnClickListener {
-            if (mediaPlayer == null){
+            if (mediaPlayer == null) {
                 mediaPlayer = MediaPlayer.create(applicationContext, R.raw.song)
             }
             mediaPlayer!!.start()
         }
+        if (checkSelfPermission(re) == PackageManager.PERMISSION_GRANTED) {
+           songsGet()
+        } else {
+            requestPermissions(arrayOf(re), 12345)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if((grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+            songsGet()
+        }
+        else{
+            requestPermissions(arrayOf(re),12345)
+        }
+    }
+
+    private fun songsGet() {
 
         requestPermissions(arrayOf(re),12345)
         val songListViewModel = SongListViewModel(applicationContext)
 
-        songListViewModel.getSongList().observe(this, Observer {songs ->
+        songListViewModel.getSongList()
+/*.observe(this, Observer { songs ->
             TODO()
-        })
-    }
+        }) */   }
 }
